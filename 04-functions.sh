e5348a58bb3fea3e2b5c5b1326aa94a5a384c981
Vivek <vivekanandareddy7677@gmail.com>
2024-03-20 20:27:36 -0500

    Example of a function usage

--- 04-functions.sh
+++ 04-functions.sh
@@ -3,3 +3,14 @@ example(){
 }
 # Calling the function
 example
+
+#Functions have their own special variables
+
+example1(){
+  echo Value of 1 - $1
+  echo "Value of * - $*"
+  echo "Value of # - $#"
+}
+
+example1 30 40 50
+example $*
