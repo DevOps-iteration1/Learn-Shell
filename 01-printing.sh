d4859dfc6b0b397047de120ed1a40494557270b4
Vivek <vivekanandareddy7677@gmail.com>
2024-03-21 19:57:11 -0500

    PROVIDING OUTPUT IN COLORS

--- 01-printing.sh
+++ 01-printing.sh
@@ -1,1 +1,26 @@
 echo Hello World
+
+#Color Printing
+
+#Colors
+#Red      -  31
+#Green    -  32
+#Yellow   -  33
+#Blue     -  34
+#Magenta  -  35
+#Cyan     -  36
+
+#Disable color - 0
+
+# syntax: echo -e "\e[COLmMESSAGE\e[0m"
+#              -e - enable color
+#              \e[COLm - start color
+#              MESSAGE - Message to be printed
+#              \e[0m   - Disable the color
+
+echo -e "\e[31mRED\e[0m"
+echo -e "\e[32mGREEN\e[0m"
+echo -e "\e[33mYELLOW\e[0m"
+echo -e "\e[34mBLUE\e[0m"
+echo -e "\e[35mMAGENTA\e[0m"
+echo -e "\e[36mCYAN\e[0m"
